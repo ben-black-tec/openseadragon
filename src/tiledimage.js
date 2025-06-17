9634e971b86fc53a6d79b2427d5156133bdb69bc
Ben Black <ben.black@techcyte.com>
2025-06-17 00:55:02 -0600

    prefetch adaptively

--- src/tiledimage.js
+++ src/tiledimage.js
@@ -1408,7 +1408,7 @@ $.extend($.TiledImage.prototype, $.EventSource.prototype, /** @lends OpenSeadrag
             let curDrawArea = drawArea;
             const DIV_RATIO = 8;
             if(this.immediateRender && currentRenderPixelRatio >= DIV_RATIO * this.minPixelRatio){
-                const AREA_RATIO = (currentRenderPixelRatio / (DIV_RATIO * this.minPixelRatio));
+                const AREA_RATIO = Math.sqrt(currentRenderPixelRatio / (DIV_RATIO * this.minPixelRatio));
                 curDrawArea = new $.Rect(curDrawArea.x - curDrawArea.width * AREA_RATIO, curDrawArea.y - curDrawArea.height * AREA_RATIO, curDrawArea.width * (1 + AREA_RATIO * 2), curDrawArea.height * (1 + AREA_RATIO * 2));
                 levelVisibility = AREA_RATIO;
             }
