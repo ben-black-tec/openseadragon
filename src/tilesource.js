dbee7b9d1e5f67bbfa6c62a7369b647e4724b659
Ben Black <ben.black@techcyte.com>
2025-06-18 14:05:18 -0600

    added back tile closing

--- src/tilesource.js
+++ src/tilesource.js
@@ -863,7 +863,6 @@ $.TileSource.prototype = {
     destroyTileCache: function (cacheObject) {
         // closes cacheObject._data if it is an ImageBitmap
         if(cacheObject._data && cacheObject._data.close){
-            console.log("Unloading tile", cacheObject);
             cacheObject._data.close();
         }
         cacheObject._data = null;
