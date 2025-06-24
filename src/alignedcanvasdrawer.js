0857f62fda1f0ec59180cebfb7896f16cb19f9a7
Benjamin Black <ben.black@techcyte.com>
2025-06-24 17:53:17 -0600

    debugging prints

--- src/alignedcanvasdrawer.js
+++ src/alignedcanvasdrawer.js
@@ -213,7 +213,7 @@ class AlignedCanvasDrawer extends OpenSeadragon.DrawerBase {
                     viewportSizeY
                 );
             }
-            console.log(this.canvas.height, viewPortWidth, this.scanvas.height, highTileRatio, this.scanvas.height / highTileRatio);
+            console.log(this.canvas.height, viewPortHeight, this.canvas.width, viewPortWidth, this.scanvas.height, highTileRatio, this.scanvas.height / highTileRatio);
             // clears background context to alpha zero
             // NOTE: observed janky behavior in certain cases,
             this.scontext.clearRect(
