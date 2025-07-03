73cf066553ef466217aa40deba02c0154ba609d2
Benjamin Black <ben.black@techcyte.com>
2025-07-02 18:05:57 -0600

    more debugging

--- src/alignedcanvasdrawer.js
+++ src/alignedcanvasdrawer.js
@@ -119,7 +119,7 @@ class AlignedCanvasDrawer extends OpenSeadragon.DrawerBase {
             this.canvas.width !== viewportSize.x ||
             this.canvas.height !== viewportSize.y
         ) {
-            console.log("Canvas changed size!", viewportSize.x, viewportSize.y, tiledImages, imageTilesList);
+            console.log("Canvas changed size!", viewportSize.x, viewportSize.y, tiledImages, imageTilesList, this.imageTilesList[0].cacheImageRecord.getData());
             this.canvas.width = viewportSize.x;
             this.canvas.height = viewportSize.y;
         }
