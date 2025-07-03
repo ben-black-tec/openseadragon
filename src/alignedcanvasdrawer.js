ad50d9278432815fc6230537b082b4ca5cd33d03
Benjamin Black <ben.black@techcyte.com>
2025-07-02 18:24:19 -0600

    more debugging

--- src/alignedcanvasdrawer.js
+++ src/alignedcanvasdrawer.js
@@ -510,12 +510,13 @@ class AlignedCanvasDrawer extends OpenSeadragon.DrawerBase {
             if ((tile.opacity && tile.opacity !== 1) || tile.opacity === 0) {
                 this.scontext.globalAlpha = tile.opacity;
             }
+            console.log()
             this.scontext.drawImage(
                 rendered,
                 0,
                 0,
-                tile.sourceBounds.width,
-                tile.sourceBounds.height,
+                rendered.width,
+                rendered.height,
                 // using rounded dest values to make sure the GPU aliases right
                 rdx,
                 rdy,
